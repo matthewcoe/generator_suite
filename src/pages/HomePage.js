47c51ed70e34e9e91dff56c893e9751727fd57ef
Matthew Coe <matthewcoe001@gmail.com>
2022-10-24 03:55:02 -0600

    HomePage creation

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import { Container } from 'reactstrap';
+
+const HomePage = () => {
+    return (
+        <Container>
+            <header>HOMEPAGE!</header>
+        </Container>
+    );
+};
+
+export default HomePage;
